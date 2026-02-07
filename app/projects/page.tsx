43bb8dffe40dc92fd27829ed2621bca47bc6b3be
fullstacknyc <myemailxd@icloud.com>
2026-02-06 19:06:32 -0500

    projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { useState, useEffect } from 'react'
-
 const ProjectsPage = () => {
   return (
     // Tik Tok Style display of projects
     <div className="max-w-3xl mx-auto p-4 mt-20">
-      <h1 className="text-3xl font-bold mb-6">Projects</h1>
-      <ul className="list-disc list-inside space-y-2 list-none">
+      <h1 className="text-3xl font-bold mb-6 text-center">Projects</h1>
+      <ul className="list-disc list-inside space-y-2 text-center">
         <li><a href="https://www.camilo777.com">Camilo777</a></li>
         <li><a href="/notes">Notes App</a></li>
         <li><a href="/diet">Diet Generator</a></li>
