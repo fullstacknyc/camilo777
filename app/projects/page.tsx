61d0307d8b54274ca0127350439241cd943e713e
fullstacknyc <myemailxd@icloud.com>
2026-02-11 17:36:51 -0500

    e

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,9 +7,6 @@ const ProjectsPage = () => {
       <h1 className="text-3xl font-bold mb-6 text-center">Projects</h1>
       <ul className="list-disc list-inside space-y-2 text-center ">
         <li><a href="https://www.camilo777.com">Camilo777</a></li>
-        <li><a href="/notes">Notes App</a></li>
-        <li><a href="/diet">Diet Generator</a></li>
-        <li><a href="/workout">Workout Generator</a></li>
         <li><a href="/scheduler">Scheduler App</a></li>
       </ul>
     </div>
