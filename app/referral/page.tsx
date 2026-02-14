dbd98de3d5b0eb506ff860d174478c942251873b
fullstacknyc <myemailxd@icloud.com>
2026-02-14 18:08:05 -0500

    coinbase

--- app/referral/page.tsx
+++ app/referral/page.tsx
@@ -5,6 +5,8 @@ const ReferralPage = () => {
         <h1 className="text-3xl font-bold mb-6 text-center m-6">Referrals</h1>
         <ul className="list-disc list-inside space-y-2 text-center ">
             <li><a href="https://gomining.com/?ref=BM4W812" target="_blank">Cloud Mining</a></li>
+            <li><a href="https://trustdice.win/faucet/?ref=u_camgoat" target="_blank">TrustDice Faucet</a></li>
+            <li><a href="https://coinbase.com/join/ZXD8P8E?src=referral-link" target="_blank">Coinbase Referral</a></li>
         </ul>
     </div>
 }
